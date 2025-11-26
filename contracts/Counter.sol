dc8444c1bddfd555cd46acd65d9ed7d3253fca0c
Deba-07 <dynamicdeba07@gmail.com>
2025-11-26 11:38:04 +0530

    add counter contract

--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -2,9 +2,19 @@
 pragma solidity ^0.8.19;
 
 contract Counter {
-    uint256 count;
+    uint256 public count;
 
-    function getCount() public{
+    // By the help of constructor we can give a default value to the count
+    // constructor() {
+    //     count = 0;
+    // }
 
+    // Don't this function as we have public identifier to the count variable
+    // function getCount() public view returns(uint){
+    //     return count;
+    // }
+
+    function incrementConut() public{
+        count++;
     }
 }
