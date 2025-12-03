5d3a08b3ba0b955c507667268a5dedd87f1f519c
Deba-07 <dynamicdeba07@gmail.com>
2025-12-03 09:03:06 +0530

    Variable and datatypes in solidity

---
+++ contracts/MyContract.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract MyContract{
+    // state variable
+    uint public myNum = 10;
+    int myInt = -10;
+    string public myString = "Hello World!!";
+    bytes32 public myBytes = "Hello";
+    address public myAddress = 0x694AA1769357215DE4FAC081bf1f309aDC325306;
+
+    // custom dataType
+    struct myStruct {
+        uint256 myUint256;
+        string myString;
+    }
+    myStruct public myData = myStruct(10, "Hello");
+
+    // Local variable
+    function getValue() public pure returns(uint) {
+        uint value = 1;
+        return value;
+    }
+}
