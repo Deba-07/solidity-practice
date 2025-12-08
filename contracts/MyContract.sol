043821079a89babb364e1b0588d6c92b2b849a04
Deba-07 <dynamicdeba07@gmail.com>
2025-12-08 10:13:30 +0530

    add mappings to the contract

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -34,4 +34,29 @@ contract MyContract{
     function valueCount() public view returns(uint){
         return values.length;
     }
+
+    // Mappings
+    mapping (uint => string) public names;
+
+    constructor() {
+        names[1] = "Deba";
+        names[2] = "Rahul";
+    }
+
+    struct Book {
+        string title;
+        string author;
+    }
+    mapping (uint => Book) public books;
+
+    function addBook(uint _id, string memory _title, string memory _author) public {
+        books[_id] = Book(_title, _author);
+    }
+
+    // Nested Mapping
+    mapping (address => mapping (uint => Book)) public myBooks;
+
+    function addMyBooks(uint _id, string memory _title, string memory _author) public {
+        myBooks[msg.sender][_id] = Book(_title, _author);
+    }
 }
