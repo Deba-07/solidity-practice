2d9187e02f7d73c1cc52e315f844be6af6d63747
Deba-07 <dynamicdeba07@gmail.com>
2025-12-16 09:10:45 +0530

    work on inheritance, factory and call contract from an another contract

---
+++ contracts/Contract.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Ownable {
+    address owner;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Must be Owner");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+}
+
+contract SecretVault {
+    string secret;
+
+    constructor(string memory _secret) {
+        secret = _secret;
+    }
+
+    function getSecret() public view returns(string memory){
+        return secret;
+    }
+}
+
+contract Contract is Ownable {
+    address secretVault;
+
+    constructor(string memory _secret) {
+        SecretVault _secretVault = new SecretVault(_secret);
+        secretVault = address(_secretVault);
+    }
+    
+    function getSecret() public view returns(string memory){
+        return SecretVault(secretVault).getSecret();
+    }
+}
